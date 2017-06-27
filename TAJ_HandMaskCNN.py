9ed0e6fc475d2ebf8359215573efea9a99c1d473
Titus John <titusaj92@gmail.com>
2017-06-27 12:45:56 -0400

    created the intial script to train of the hand mask data

---
+++ TAJ_HandMaskCNN.py
@@ -0,0 +1,13 @@
+# Titus John
+# 6-27-2017
+# Leventhal Lab, University of Michigan
+#-----------------------------
+#Input
+#Input images - these are the raw color images
+#Labled images - these are the segmented ground truth images
+
+#Output
+
+#------------------------------
+#Import the data
+import tensorflow as tf 
