6b4f84b1f55f3c11063f9a01e854f8b30eed5029
Titus John <titusaj92@gmail.com>
2017-06-28 17:04:56 -0400

    Got network together for traning

--- TAJ_DataImport.py
+++ TAJ_DataImport.py
@@ -10,34 +10,36 @@
 
 #------------------------------
 #This scripts is used to import the data into the workspace for data preprocessing
+from __future__ import print_function
 
 import os
 import numpy as np
 
-from skiimaage.io import imsave, imread
+from skimage.io import imsave, imread
 
-data_path ='raw'
+data_path ='/Users/titusjohn/Documents/HandMaskCNN/'
 
 image_rows = 420
 image_cols = 580
 
-def create_train_data();
+def create_train_data():
     train_data_path = os.path.join(data_path, 'train')
-    iamges = os.listdir(train_data_path)
+    images = os.listdir(train_data_path)
     total = len(images) / 2
 
-    imgs = np.ndarray((total, iamges_rows, image_cols) , dtype=np.uint8)
-    image_mask = np.ndarray((total, images_rows, image_cols), dtype=unint.8)
+    imgs = np.ndarray((total, image_rows, image_cols), dtype=np.uint8)
+    imgs_mask = np.ndarray((total, image_rows, image_cols), dtype=np.uint8)
 
     i = 0
     print('-'*30)
-    print('Creating training images')
-    print('i'*30)
-    for image_name in images
-        if 'mask' in image_name;
+    print('Creating training images...')
+    print('-'*30)
+    for image_name in images:
+        if 'mask' in image_name:
             continue
         image_mask_name = image_name.split('.')[0] + '_mask.tif'
-        imh = imread(os.path.join(train_data_path, image_name) as_grey = True)
+        img = imread(os.path.join(train_data_path, image_name), as_grey=True)
+        img_mask = imread(os.path.join(train_data_path, image_mask_name), as_grey=True)
 
         img = np.array([img])
         img_mask = np.array([img_mask])
@@ -45,10 +47,59 @@ def create_train_data();
         imgs[i] = img
         imgs_mask[i] = img_mask
 
-        if i%100 ==0
+        if i % 100 == 0:
             print('Done: {0}/{1} images'.format(i, total))
-            i += 1
-    print('Done loading iamges.')
+        i += 1
+    print('Loading done.')
+
+
     np.save('imgs_train.npy', imgs)
-    np.save('imgs_mask_train.npy',imgs_mask)
-    print('Saving training images to .npy files.')
+    np.save('imgs_mask_train.npy', imgs_mask)
+    print('Saving to .npy files done.')
+
+
+def load_train_data():
+    imgs_train = np.load('imgs_train.npy')
+    imgs_mask_train = np.load('imgs_mask_train.npy')
+    return imgs_train, imgs_mask_train
+
+
+def create_test_data():
+    train_data_path = os.path.join(data_path, 'test')
+    images = os.listdir(train_data_path)
+    total = len(images)
+
+    imgs = np.ndarray((total, image_rows, image_cols), dtype=np.uint8)
+    imgs_id = np.ndarray((total, ), dtype=np.int32)
+
+    i = 0
+    print('-'*30)
+    print('Creating test images...')
+    print('-'*30)
+    for image_name in images:
+        img_id = int(image_name.split('.')[0])
+        img = imread(os.path.join(train_data_path, image_name), as_grey=True)
+
+        img = np.array([img])
+
+        imgs[i] = img
+        imgs_id[i] = img_id
+
+        if i % 100 == 0:
+            print('Done: {0}/{1} images'.format(i, total))
+        i += 1
+    print('Loading done.')
+
+    np.save('imgs_test.npy', imgs)
+    np.save('imgs_id_test.npy', imgs_id)
+    print('Saving to .npy files done.')
+
+
+def load_test_data():
+    imgs_test = np.load('imgs_test.npy')
+    imgs_id = np.load('imgs_id_test.npy')
+    return imgs_test, imgs_id
+
+if __name__ == '__main__':
+    create_train_data()
+    create_test_data()
--- TAJ_HandMaskCNN.py
+++ TAJ_HandMaskCNN.py
@@ -10,88 +10,156 @@
 
 #------------------------------
 #This is an image classifier trained utilizing for segmentation classification
+from __future__ import print_function
+
 import os
 from skimage.transform import resize
 from skimage.io import imsave
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras import optimizers
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+
+from TAJ_DataImport import load_train_data, load_test_data
+
+K.set_image_data_format('channels_last')  # TF dimension ordering in this code
+
+img_rows = 96
+img_cols = 96
+
+smooth = 1.
 
 
-#import the train and testing data
-from data import load_train_data, load_test_data
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 
-#dimensions of the images used in training
-img_width, img_height = 150, 150
-
-train_data_dir = 'data/train'
-validation_data_dir ='data/validation'
-
-# Import data
-# Used to rescale the pixel values from [0,255] to [0,1] interval
-datagen =  ImageDataGenerator(rescale = 1./255)
-
-#automatically retrive images and their classes for train and validation sets
-train_generator = datagen.flow_from_directory(
-    train_data_dir,
-    target_size = (img_width, img_height),
-    batch_size = 16,
-    class_mode = 'binary'
-)
-
-validation_generator = datagen.flow_from_directory(
-    validation_data_dir,
-    target_size = (img_width, img_height),
-    batch_size = 32,
-    class_mode = 'binary'
-)
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
 
 
-#Small CNN
-#Model architecture definition
-model = Sequential()
+def get_unet():
+    inputs = Input((img_rows, img_cols, 1))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-model.add(Convolution2D(32, 3,3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['mae', 'acc'])
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+
+    return model
 
 
-#Training
-nb_epoch = 30
-nb_train_samples = 2048
-nb_validation_samples = 832
+def preprocess(imgs):
+    imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols), dtype=np.uint8)
+    for i in range(imgs.shape[0]):
+        imgs_p[i] = resize(imgs[i], (img_cols, img_rows), preserve_range=True)
 
-model.fit_generator(
-    train_generator,
-    samples_per_epoch = nb_train_samples,
-    nb_epoch= nb_epoch,
-    validation_data = validation_generator,
-    nb_val_samples = nb_validation_samples)
+    imgs_p = imgs_p[..., np.newaxis]
+    return imgs_p
 
 
-model.save_weights('models/basic_cnn_30_epochs.h5')
+def train_and_predict():
+    print('-'*30)
+    print('Loading and preprocessing train data...')
+    print('-'*30)
+    imgs_train, imgs_mask_train = load_train_data()
 
-model.evaluate_generator(validation_generator, nb_validation_samples)
+    imgs_train = preprocess(imgs_train)
+    imgs_mask_train = preprocess(imgs_mask_train)
+
+    imgs_train = imgs_train.astype('float32')
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean
+    imgs_train /= std
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+    print('-'*30)
+    print('Creating and compiling model...')
+    print('-'*30)
+    model = get_unet()
+    model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
+
+    print('-'*30)
+    print('Fitting model...')
+    print('-'*30)
+    model.fit(imgs_train, imgs_mask_train, batch_size=32, nb_epoch=20, verbose=1, shuffle=True,
+              validation_split=0.2,
+              callbacks=[model_checkpoint])
+
+    print('-'*30)
+    print('Loading and preprocessing test data...')
+    print('-'*30)
+    imgs_test, imgs_id_test = load_test_data()
+    imgs_test = preprocess(imgs_test)
+
+    imgs_test = imgs_test.astype('float32')
+    imgs_test -= mean
+    imgs_test /= std
+
+    print('-'*30)
+    print('Loading saved weights...')
+    print('-'*30)
+    model.load_weights('weights.h5')
+
+    print('-'*30)
+    print('Predicting masks on test data...')
+    print('-'*30)
+    imgs_mask_test = model.predict(imgs_test, verbose=1)
+    np.save('imgs_mask_test.npy', imgs_mask_test)
+
+    print('-' * 30)
+    print('Saving predicted masks to files...')
+    print('-' * 30)
+    pred_dir = 'preds'
+    if not os.path.exists(pred_dir):
+        os.mkdir(pred_dir)
+    for image, image_id in zip(imgs_mask_test, imgs_id_test):
+        image = (image[:, :, 0] * 255.).astype(np.uint8)
+        imsave(os.path.join(pred_dir, str(image_id) + '_pred.png'), image)
+
+if __name__ == '__main__':
+    train_and_predict()
