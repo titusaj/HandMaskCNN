286978922deafd59f6292039bf88a02f19266869
Titus John <titusaj92@gmail.com>
2017-07-09 23:07:39 -0400

    need to figure out how to train a cnn to do color mask regnition wq quit quit()

--- PawSegmentationMatlab/SingleVideoSegmentation.m
+++ PawSegmentationMatlab/SingleVideoSegmentation.m
@@ -19,30 +19,30 @@ function SingleVideoSegmentation(videoFileName)
     
     for i = 1:numFrames %loop through the frams of the video to extract the
          rgbImage = read(video, i);
-%         [pawMaskLargestBlob,oneBlobCheck]= pawSegmentationFront(rgbImage);
-%         
+        [pawMaskLargestBlob,oneBlobCheck]= pawSegmentationFront(rgbImage);
+        
          i
-% %         figure(1)
-% %         imshow(rgbImage)
-% %         
-% %          figure(2)
-% %         imshow(pawMaskLargestBlob)
+%         figure(1)
+%         imshow(rgbImage)
 %         
-%         %Convert the images to uint8
-         rgbImage = im2uint8(rgb2gray(rgbImage));
-%         pawMaskLargestBlob = im2uint8(pawMaskLargestBlob);
-%         
-%         %filename for rgb image
+%          figure(2)
+%         imshow(pawMaskLargestBlob)
+        
+        %Convert the images to uint8
+         rgbImage = im2uint8(rgb2gray(decorrstretch(rgbImage)));
+        pawMaskLargestBlob = im2uint8(pawMaskLargestBlob);
+        
+        %filename for rgb image
          rgbFilename = strcat('1_',num2str(i),'.tif');
          imwrite(rgbImage,rgbFilename); 
-%         
-%        
-%         
-%         %filename for mask of paw
-%         maskFilename = strcat('1_',num2str(i),'_mask.tif');
-%         imwrite(pawMaskLargestBlob,maskFilename); 
-%         
-%         
+        
+       
+        
+        %filename for mask of paw
+        maskFilename = strcat('1_',num2str(i),'_mask.tif');
+        imwrite(pawMaskLargestBlob,maskFilename); 
+        
+        
         
     end
 
