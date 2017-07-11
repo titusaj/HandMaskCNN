ca925f1d67b24d451a22fcde3e89fc4436323620
Titus John <titusaj92@gmail.com>
2017-07-10 23:34:46 -0400

    working on adding color images to net

---
+++ pawCNN.py
@@ -0,0 +1,14 @@
+# Titus John
+# 6-27-2017
+# Leventhal Lab, University of Michigan
+#-----------------------------
+#Input
+#Input images - these are the raw color images
+#Labled images - these are the segmented ground truth images
+
+#Output
+
+#------------------------------
+#This is an image classifier trained utilizing for segmentation classification
+
+#Have to figure out how to feed color image into net
